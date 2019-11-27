3c4e0cacb67d480130036cbe3baae6b0f0635312
Steve Strong <steve@id3as.co.uk>
2019-11-27 16:25:02 +0000

    Added fromFoldable and foldableWithIndex

--- src/Erl/Data/Map.purs
+++ src/Erl/Data/Map.purs
@@ -7,14 +7,17 @@ module Erl.Data.Map
   , lookup
   , values
   , mapWithKey
+  , fromFoldable
   ) where
 
 import Prelude
 
-import Data.Foldable (class Foldable, foldr)
+import Data.Foldable (class Foldable, foldl, foldr)
+import Data.FoldableWithIndex (class FoldableWithIndex)
 import Data.Function.Uncurried (Fn2, mkFn2)
 import Data.Maybe (Maybe(..))
 import Data.Traversable (class Traversable, sequenceDefault)
+import Data.Tuple (Tuple(..))
 import Erl.Data.List (List)
 
 foreign import data Map :: Type -> Type -> Type
@@ -56,17 +59,35 @@ fold = foldMImpl ((#))
 -- | Fold the keys and values of a map, accumulating values using some
 -- | `Monoid`.
 foldMap :: forall a b m. Monoid m => (a -> b -> m) -> Map a b -> m
-foldMap f = fold (\acc k v -> acc <> f k v) mempty
+foldMap f = fold (\acc k v -> f k v <> acc) mempty
 
 -- | Fold the keys and values of a map, accumulating values and effects in
 -- | some `Monad`.
 foldM :: forall a b m z. Monad m => (z -> a -> b -> m z) -> z -> Map a b -> m z
 foldM f z = foldMImpl bind f (pure z)
 
+-- | Convert any foldable collection of key/value pairs to a map.
+-- | On key collision, later values take precedence over earlier ones.
+fromFoldable :: forall f k v. Ord k => Foldable f => f (Tuple k v) -> Map k v
+fromFoldable = foldl (\m (Tuple k v) -> insert k v m) empty
+
+--foldr :: forall a b. (a -> b -> b) -> b -> f a -> b
+--foldl :: forall a b. (b -> a -> b) -> b -> f a -> b
+--foldMap :: forall a m. Monoid m => (a -> m) -> f a -> m
 instance foldableMap :: Foldable (Map a) where
-  foldl f = fold (\z _ -> f z)
   foldr f z m = foldr f z (values m)
+  foldl f = fold (\z _ -> f z)
   foldMap f = foldMap (const f)
+
+
+--foldrWithIndex :: forall a b. (i -> a -> b -> b) -> b -> f a -> b
+--foldlWithIndex :: forall a b. (i -> b -> a -> b) -> b -> f a -> b
+--foldMapWithIndex :: forall a m. Monoid m => (i -> a -> m) -> f a -> m
+
+instance foldableWithIndexMap :: FoldableWithIndex a (Map a) where
+  foldrWithIndex f = fold (\b i a -> f i a b)
+  foldlWithIndex f = fold (\b i a -> f i b a)
+  foldMapWithIndex = foldMap
 
 instance traversableMap :: Traversable (Map a) where
   traverse f ms = fold (\acc k v -> flip (insert k) <$> acc <*> f v) (pure empty) ms
