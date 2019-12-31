eea0a3cfeb744949e91aab28133f802e020bcb24
Adrian Roe <adrian@id3as.co.uk>
2019-12-31 15:24:19 +0000

    Slight efficiency increase and a bunch of new methods

--- src/Erl/Data/Map.erl
+++ src/Erl/Data/Map.erl
@@ -1,17 +1,22 @@
 -module(erl_data_map@foreign).
--export([empty/0,
-         isEmpty/1,
-         size/1,
-         insert/3,
-         lookupImpl/4,
-         values/1,
-         keys/1,
-         member/2,
-         mapImpl/2,
-         mapWithKeyImpl/2,
-         delete/2,
-         difference/2,
-         foldMImpl/4]).
+-export([ delete/2
+        , difference/2
+        , empty/0
+        , filterWithKeyImpl/2
+        , foldImpl/3
+        , foldMImpl/4
+        , insert/3
+        , isEmpty/1
+        , keys/1
+        , lookupImpl/4
+        , mapImpl/2
+        , mapWithKeyImpl/2
+        , member/2
+        , size/1
+        , toUnfoldableImpl/2
+        , toUnfoldableUnorderedImpl/2
+        , values/1
+]).
 
 empty() -> #{}.
 isEmpty(M) -> M =:= #{}.
@@ -20,10 +25,12 @@ size(M) -> maps:size(M).
 
 insert(K, V, M) -> M#{ K => V}.
 
+filterWithKeyImpl(F, M) -> maps:filter(F, M).
+
 lookupImpl(Nothing, Just, K, M) ->
-    case maps:is_key(K, M) of
-        true -> Just(maps:get(K, M));
-        false -> Nothing
+    case maps:find(K, M) of
+      {ok, Value} -> Just(Value);
+      error -> Nothing
     end.
 
 values(M) -> maps:values(M).
@@ -39,6 +46,9 @@ foldMImpl(Bind, F, MZ, M) ->
         (Bind(Acc))(fun (Z) -> ((F(Z))(K))(V) end)
     end, MZ, M).
 
+foldImpl(F, MZ, M) ->
+    maps:fold(F, MZ, M).
+
 member(K, M) -> maps:is_key(K, M).
 
 delete(K, M) -> maps:remove(K, M).
@@ -49,3 +59,9 @@ difference(M1, M2) ->
             end,
             M1,
             M2).
+
+toUnfoldableImpl(Tuple, M) ->
+  [Tuple(K, V) || {K, V} <- lists:sort(maps:to_list(M))].
+
+toUnfoldableUnorderedImpl(Tuple, M) ->
+  [Tuple(K, V) || {K, V} <- maps:to_list(M)].
